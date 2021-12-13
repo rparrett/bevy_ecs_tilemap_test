5d8b17cafb68b5f11b0bba14d962d6d67cc2fd7a
Rob Parrett <robparrett@gmail.com>
2021-12-13 16:39:25 -0700

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use bevy::{
+    prelude::{App, AssetServer, Commands, GlobalTransform, Res, Transform},
+    render2::camera::OrthographicCameraBundle,
+    window::WindowDescriptor,
+    PipelinedDefaultPlugins,
+};
+use bevy_ecs_tilemap::prelude::*;
+
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>, mut map_query: MapQuery) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    let texture_handle = asset_server.load("taipo-tilesheet.png");
+
+    // Create map entity and component:
+    let map_entity = commands.spawn().id();
+    let mut map = Map::new(0u16, map_entity);
+
+    // Creates a new layer builder with a layer entity.
+    let (mut layer_builder, _) = LayerBuilder::new(
+        &mut commands,
+        LayerSettings::new(
+            MapSize(2, 2),
+            ChunkSize(8, 8), // 62, 62 (or greater values) mysteriously unbreaks things
+            TileSize(32.0, 32.0),
+            TextureSize(512.0, 512.0),
+        ),
+        0u16,
+        0u16,
+    );
+
+    layer_builder.set_all(TileBundle::default());
+
+    // Builds the layer.
+    // Note: Once this is called you can no longer edit the layer until a hard sync in bevy.
+    let layer_entity = map_query.build_layer(&mut commands, layer_builder, texture_handle);
+
+    // Required to keep track of layers for a map internally.
+    map.add_layer(&mut commands, 0u16, layer_entity);
+
+    // Spawn Map
+    // Required in order to use map_query to retrieve layers/tiles.
+    commands
+        .entity(map_entity)
+        .insert(map)
+        .insert(Transform::from_xyz(-128.0, -128.0, 0.0))
+        .insert(GlobalTransform::default());
+}
+
+fn main() {
+    // env_logger::Builder::from_default_env()
+    //     .filter_level(log::LevelFilter::Info)
+    //     .init();
+
+    App::new()
+        .insert_resource(WindowDescriptor {
+            width: 1270.0,
+            height: 720.0,
+            title: String::from("Map Example"),
+            ..Default::default()
+        })
+        .add_plugins(PipelinedDefaultPlugins)
+        .add_plugin(TilemapPlugin)
+        .add_startup_system(startup)
+        .run();
+}
