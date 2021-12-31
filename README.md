90201d215f525f12c1488bee5010bc516c6ecfcc
Rob Parrett <robparrett@gmail.com>
2021-12-31 12:31:01 -0700

    Test wasm

---
+++ README.md
@@ -0,0 +1,15 @@
+# Instructions
+
+## wasm
+
+```
+cargo make serve
+cargo make serve --profile release
+```
+
+## native
+
+```
+cargo make run
+cargo make run --profile release
+```
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-use bevy::{
-    prelude::{App, AssetServer, Commands, GlobalTransform, Res, Transform},
-    render2::camera::OrthographicCameraBundle,
-    window::WindowDescriptor,
-    PipelinedDefaultPlugins,
-};
+use bevy::{prelude::*, render::render_resource::TextureUsages};
 use bevy_ecs_tilemap::prelude::*;
 
 fn startup(mut commands: Commands, asset_server: Res<AssetServer>, mut map_query: MapQuery) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
-    let texture_handle = asset_server.load("taipo-tilesheet.png");
+    let texture_handle = asset_server.load("tiles.png");
 
     // Create map entity and component:
     let map_entity = commands.spawn().id();
@@ -19,16 +14,23 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>, mut map_query
     let (mut layer_builder, _) = LayerBuilder::new(
         &mut commands,
         LayerSettings::new(
-            MapSize(2, 2),
-            ChunkSize(8, 8), // 62, 62 (or greater values) mysteriously unbreaks things
-            TileSize(32.0, 32.0),
-            TextureSize(512.0, 512.0),
+            MapSize(5, 2),
+            ChunkSize(16, 16),
+            TileSize(16.0, 16.0),
+            TextureSize(96.0, 16.0),
+            //TextureSize(96.0, 16.0),
         ),
         0u16,
         0u16,
     );
 
-    layer_builder.set_all(TileBundle::default());
+    layer_builder.set_all(TileBundle {
+        tile: Tile {
+            texture_index: 1,
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 
     // Builds the layer.
     // Note: Once this is called you can no longer edit the layer until a hard sync in bevy.
@@ -42,24 +44,49 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>, mut map_query
     commands
         .entity(map_entity)
         .insert(map)
-        .insert(Transform::from_xyz(-128.0, -128.0, 0.0))
+        .insert(Transform::from_xyz(-640.0, -256.0, 0.0))
         .insert(GlobalTransform::default());
 }
 
 fn main() {
-    // env_logger::Builder::from_default_env()
-    //     .filter_level(log::LevelFilter::Info)
-    //     .init();
+    let mut app = App::new();
 
-    App::new()
-        .insert_resource(WindowDescriptor {
-            width: 1270.0,
-            height: 720.0,
-            title: String::from("Map Example"),
-            ..Default::default()
-        })
-        .add_plugins(PipelinedDefaultPlugins)
+    #[cfg(target_arch = "wasm32")]
+    app.insert_resource(WindowDescriptor {
+        width: 720.,
+        height: 480.,
+        canvas: Some("#bevy-canvas".to_string()),
+        ..Default::default()
+    });
+    #[cfg(not(target_arch = "wasm32"))]
+    app.insert_resource(WindowDescriptor {
+        width: 720.,
+        height: 480.,
+        ..Default::default()
+    });
+
+    app.add_plugins(DefaultPlugins)
         .add_plugin(TilemapPlugin)
         .add_startup_system(startup)
+        .add_system(set_texture_filters_to_nearest)
         .run();
+}
+
+pub fn set_texture_filters_to_nearest(
+    mut texture_events: EventReader<AssetEvent<Image>>,
+    mut textures: ResMut<Assets<Image>>,
+) {
+    // quick and dirty, run this for all textures anytime a texture is created.
+    for event in texture_events.iter() {
+        match event {
+            AssetEvent::Created { handle } => {
+                if let Some(mut texture) = textures.get_mut(handle) {
+                    texture.texture_descriptor.usage = TextureUsages::TEXTURE_BINDING
+                        | TextureUsages::COPY_SRC
+                        | TextureUsages::COPY_DST;
+                }
+            }
+            _ => (),
+        }
+    }
 }
